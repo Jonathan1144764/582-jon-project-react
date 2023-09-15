872fe9673154c6d510806516536ecc0272e61236
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-14 20:16:21 -0400

    Added fetch to get users and display

--- src/components/ManageUsers.js
+++ src/components/ManageUsers.js
@@ -1,24 +1,44 @@
+import { useState, useEffect } from "react";
 import UserList from "./UserList";
 
 export default function ManageUsers() {
-  let allUsers = [];
+  const [users, setUsers] = useState([]);
 
-  function handleFetchUsers() {
-    allUsers = [];
-    fetch("https://special-doodle-r949xwgp9jpf5w56-3000.app.github.dev/")
-      .then((response) => response.json())
-      .then((json) => {
-        for (let user of json) {
-          allUsers.push(user);
-        }
-      });
-    console.log(allUsers);
-  }
-  handleFetchUsers();
+  useEffect(() => {
+    // async function handleLoadUsers() {
+    //   const allUsers = await handleFetchUsers();
+    //   setUsers(allUsers);
+    // }
+    const handleLoadUsers = async () => {
+      const allUsers = await handleFetchUsers();
+      setUsers(allUsers);
+    };
+
+    handleLoadUsers();
+
+    // handleFetchUsers();
+  }, []);
+
+  // async function handleFetchUsers() {
+  //   await fetch("https://special-doodle-r949xwgp9jpf5w56-3000.app.github.dev/")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       // return json;
+  //       setUsers(json);
+  //     });
+  // }
+
+  const handleFetchUsers = async () => {
+    const res = await fetch(
+      "https://special-doodle-r949xwgp9jpf5w56-3000.app.github.dev/"
+    );
+    const userData = await res.json();
+    return userData;
+  };
 
   return (
     <div className="user-management">
-      <UserList users={allUsers} />
+      <UserList users={users} />
     </div>
   );
 }
