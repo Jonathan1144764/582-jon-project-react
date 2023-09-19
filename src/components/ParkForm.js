2ff9045c8946fc7c562ae64567f933eeb58bb79f
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-18 21:15:16 -0400

    Created park form component

---
+++ src/components/ParkForm.js
@@ -0,0 +1,10 @@
+import ParkNameInput from "./ParkNameInput";
+
+export default function ParkForm() {
+  return (
+    <form className="park-form">
+      <h2>Add Park</h2>
+      <ParkNameInput />
+    </form>
+  );
+}
