c7b469289093e0fd895745cd6c0eba76e124c4b9
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-19 14:53:44 -0400

    Created park image input component

--- src/components/ParkForm.js
+++ src/components/ParkForm.js
@@ -1,10 +1,14 @@
 import ParkNameInput from "./ParkNameInput";
+import ParkImageInput from "./ParkImageInput";
 
 export default function ParkForm() {
   return (
     <form className="park-form">
       <h2>Add Park</h2>
-      <ParkNameInput />
+      <div className="name-image-inputs-wrapper">
+        <ParkNameInput />
+        <ParkImageInput />
+      </div>
     </form>
   );
 }
---
+++ src/components/ParkImageInput.js
@@ -0,0 +1,8 @@
+export default function ParkImageInput() {
+  return (
+    <div className="input-wrapper">
+      <label htmlFor="image">Park image: </label>
+      <input type="text" name="image" id="park=image-input" />
+    </div>
+  );
+}
