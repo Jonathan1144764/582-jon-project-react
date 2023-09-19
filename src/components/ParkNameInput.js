66bb30560d1c6b3c4386663f73d26a90a3eb2de9
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-18 21:12:44 -0400

    created park name input component

---
+++ src/components/ParkNameInput.js
@@ -0,0 +1,8 @@
+export default function ParkNameInput() {
+  return (
+    <div className="input-wrapper">
+      <label htmlFor="name">Park name: </label>
+      <input type="text" name="name" id="park-name-input" />
+    </div>
+  );
+}
