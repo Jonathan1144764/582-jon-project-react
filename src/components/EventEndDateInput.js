f0902f556cc58d1639133c138348f6dbcab554be
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-28 19:48:01 -0400

    Created event end date input component

---
+++ src/components/EventEndDateInput.js
@@ -0,0 +1,8 @@
+export default function EventEndDateInput() {
+  return (
+    <>
+      <label htmlFor="end-date">End date: </label>
+      <input type="date" name="end-date" id="end-date-input" />
+    </>
+  );
+}
