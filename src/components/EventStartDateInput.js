ebb2d8ab9a2fe581f8d3c59f633bc6836708d33d
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-09-28 19:46:20 -0400

    Created event start date input

---
+++ src/components/EventStartDateInput.js
@@ -0,0 +1,8 @@
+export default function EventStartDateInput() {
+  return (
+    <>
+      <label htmlFor="start-date">Start date: </label>
+      <input type="date" name="start-date" id="start-date-input" />
+    </>
+  );
+}
