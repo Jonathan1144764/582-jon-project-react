812a7ac52afacb894f0794d48eed9eb1006203d5
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-10-12 21:20:13 -0400

    Changed title and added text

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import MainHeader from "../components/MainHeader";
 import AdminNav from "../components/AdminNav";
 
 export default function Admin() {
+  let location = useLocation().pathname;
+
   return (
     <>
       <MainHeader />
       <AdminNav />
-      <h2>This is Admin page</h2>
+      {location === "/admin" && (
+        <>
+          <h2>Welcome admin!</h2>
+          <p>Let's get to work!</p>
+        </>
+      )}
       <section className="admin-dashboard">
         <div className="container">
           <Outlet />
