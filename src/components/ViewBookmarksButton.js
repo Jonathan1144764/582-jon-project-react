7d8a1cb0142551243679fd78ff9d1503b0e3bf28
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-10-16 09:19:33 -0400

    Created button to view bookmarks

---
+++ src/components/ViewBookmarksButton.js
@@ -0,0 +1,7 @@
+// import { useSelector } from "react-redux";
+
+export default function ViewBookmarksButton() {
+  // const { user } = useSelector((store) => store.user);
+
+  return <button className="view-bookmarks-btn">View your events</button>;
+}
