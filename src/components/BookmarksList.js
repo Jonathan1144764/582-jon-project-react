88c21ff0aed0ea5faa31ee294c518eec4f136773
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-10-17 13:45:43 -0400

    Added props to render in EventItem

--- src/components/BookmarksList.js
+++ src/components/BookmarksList.js
@@ -1,12 +1,17 @@
 import ExitWindow from "./ExitWindow";
+import EventItem from "./EventItem";
 
 export default function BookmarksList({ isShown, exitWindow, userEvents }) {
-  console.log(userEvents);
   if (isShown) {
     return (
       <div className="bookmark-window">
         <h3>Your events</h3>
         <ExitWindow exitWindow={exitWindow} />
+        <div className="bookmarks-grid">
+          {userEvents.map((userEvent) => {
+            return <EventItem key={userEvent.id} userEvent={userEvent} />;
+          })}
+        </div>
       </div>
     );
   }
--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -1,3 +1,13 @@
-export default function EventItem() {
-  return <div className="bookmarked-events"></div>;
+export default function EventItem({ userEvent }) {
+  return (
+    <div className="bookmarked-events">
+      <h3>{userEvent.eventName}</h3>
+      <p className="event-location">{userEvent.eventLocation}</p>
+      <p>
+        <span className="event-time">From: {userEvent.eventStartDate}</span>
+        <span className="event-time">To: {userEvent.eventEndDate}</span>
+      </p>
+      <p className="event-description">{userEvent.eventDescription}</p>
+    </div>
+  );
 }
