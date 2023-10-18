4cc43aac7f9be3e09d3a86c2c14f8f6705deadbb
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-10-17 20:12:49 -0400

    Added props and ParkItem

--- src/components/ParkList.js
+++ src/components/ParkList.js
@@ -1,3 +1,11 @@
-export default function ParkList() {
-  return <div className="park-list-grid"></div>;
+import ParkItem from "./ParkItem";
+
+export default function ParkList({ allParks, allEvents }) {
+  return (
+    <div className="park-list-grid">
+      {allParks.map((park) => {
+        return <ParkItem key={park.id} park={park} allEvents={allEvents} />;
+      })}
+    </div>
+  );
 }
