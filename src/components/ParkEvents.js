f4e36dc332bb6a2c16cb2c0658655c95f84fde17
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-10-18 13:54:21 -0400

    Rendered props

--- src/components/ParkEvents.js
+++ src/components/ParkEvents.js
@@ -1,3 +1,13 @@
 export default function ParkEvents({ event }) {
-  return <div className="park-event"></div>;
+  return (
+    <div className="park-event">
+      <h3>{event.eventName}</h3>
+      <p className="event-location">{event.eventLocation}</p>
+      <p>
+        <span className="event-time">From: {event.eventStartDate}</span>
+        <span className="event-time">To: {event.eventEndDate}</span>
+      </p>
+      <p className="event-description">{event.eventDescription}</p>
+    </div>
+  );
 }
