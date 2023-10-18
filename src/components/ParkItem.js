6d2dfc08b2bd0a122e09dd9a20f0eb47e790adab
Jonathan1144764 <1144764@edu.vaniercollege.qc.ca>
2023-10-17 21:30:10 -0400

    Rendered props in ParkItem

--- src/components/ParkItem.js
+++ src/components/ParkItem.js
@@ -1,3 +1,18 @@
-export default function ParkItem() {
-  return <div className="park"></div>;
+export default function ParkItem({ park, allEvents }) {
+  return (
+    <div className="park">
+      <div
+        className="park-image"
+        style={{ backgroundImage: "url(" + park.parkImage + ")" }}
+      ></div>
+      <h3>{park.parkName}</h3>
+      <p>{park.parkStatus}</p>
+      <ul>
+        <li>Soccer fields: {park.soccerfields}</li>
+        <li>Baseball diamonds: {park.baseballDiamonds}</li>
+        <li>Chalets: {park.parkChalets}</li>
+        <li>Playgrounds: {park.parkPlaygrounds}</li>
+      </ul>
+    </div>
+  );
 }
